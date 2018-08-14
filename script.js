355630f166d707a9ff584cd93e7d93928a3966a4
mceraolo <42218246+mceraolo@users.noreply.github.com>
2018-08-14 12:49:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -23,32 +23,7 @@ $(document).ready(function() {
     }
   });
 
-  $('.gallery-list-item').click(function() {
-    let value = $(this).attr('data-filter');
-    if(value === 'all') {
-      $('.filter').show(300);
-    } else {
-      $('.filter').not('.' + value).hide(300);
-      $('.filter').filter('.' + value).show(300);
-    }
-  });
 
-  $('.gallery-list-item').click(function() {
-    $(this).addClass('active-item').siblings().removeClass('active-item');
-  });
-
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if(position >= 4300) {
-      $('.card-1').addClass('moveFromLeft');
-      $('.card-2').addClass('moveFromBottom');
-      $('.card-3').addClass('moveFromRight');
-    } else {
-      $('.card-1').removeClass('moveFromLeft');
-      $('.card-2').removeClass('moveFromBottom');
-      $('.card-3').removeClass('moveFromRight');
-    }
-  });
 
   $('#Guestmodal').on('shown.bs.modal', function() {
     $('#myInput').focus()
